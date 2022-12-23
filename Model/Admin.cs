5f161d09984f978745ee245fd26617663d8cf027
Denys Balan <89107241+hogg21@users.noreply.github.com>
2022-12-23 09:10:01 +0200

    Add files via upload

--- Model/Admin.cs
+++ Model/Admin.cs
@@ -6,6 +6,17 @@ namespace first1 {
             protected string _password { get; set; }
 
         string[] banwords = { "Ідіоти", "Йолопи" };
+
+        string[] users_list = { "Ivan", "Ursa", "Petya" };
+
+        private void button_signup() {
+            Admin hogg = new Admin();
+            bool regAdmin = false;
+
+            if(regAdmin == true) {
+                hogg.SignAdmin("hogg", "hhhghghg");
+            }
+        }
         private void SignIn(string login, string password) {
             login = _login;
             password = _password;
@@ -18,10 +29,12 @@ namespace first1 {
                     Console.WriteLine("Введений невірний логін чи пароль");
                 }
                 else
-                {
-                    Console.WriteLine("Ви увійшли в систему");
-                }
+            {
+                Console.WriteLine("Ви увійшли в систему");
+                button_signup();
+                Console.WriteLine("Користувачі онлайн: {users_list.Length}", users_list.Length);
             }
+        }
             private void banUser(string word) {
             for (int i = 0; i < word.Length; i++) {
                 if(word.IndexOf("1") == banwords.Length) {
